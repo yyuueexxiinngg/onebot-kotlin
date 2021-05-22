f54db6b535f893f8f8a8d96bbb5a52e1511b2a95
yyuueexxiinngg <yyuueexxiinngg@outlook.com>
2021-05-22 14:52:12 +0800

    Fix StackOverflow in ApiParamUtils

--- onebot-mirai/src/main/kotlin/com/github/yyuueexxiinngg/onebot/util/ApiParamsUtils.kt
+++ onebot-mirai/src/main/kotlin/com/github/yyuueexxiinngg/onebot/util/ApiParamsUtils.kt
@@ -6,14 +6,19 @@ import kotlinx.serialization.json.jsonPrimitive
 
 typealias ApiParams = Map<String, JsonElement>
 
-internal val JsonElement?.int: Int get() = this?.jsonPrimitive?.int ?: throw IllegalArgumentException()
-internal val JsonElement?.intOrNull: Int? get() = this?.jsonPrimitive?.intOrNull
+internal val JsonElement?.int: Int get() = this?.jsonPrimitive?.content?.toInt() ?: throw IllegalArgumentException()
+internal val JsonElement?.intOrNull: Int? get() = this?.jsonPrimitive?.content?.toIntOrNull()
 
-internal val JsonElement?.long: Long get() = this?.jsonPrimitive?.long ?: throw IllegalArgumentException()
-internal val JsonElement?.longOrNull: Long? get() = this?.jsonPrimitive?.longOrNull
+internal val JsonElement?.long: Long get() = this?.jsonPrimitive?.content?.toLong() ?: throw IllegalArgumentException()
+internal val JsonElement?.longOrNull: Long? get() = this?.jsonPrimitive?.content?.toLongOrNull()
 
-internal val JsonElement?.booleanOrNull: Boolean? get() = this?.jsonPrimitive?.booleanOrNull
+internal val JsonElement?.booleanOrNull: Boolean? get() = this?.jsonPrimitive?.content?.toBooleanStrictOrNull()
 
 internal val JsonElement?.string: String get() = this?.jsonPrimitive?.content ?: throw IllegalArgumentException()
 internal val JsonElement?.stringOrNull: String? get() = this?.jsonPrimitive?.contentOrNull
 
+internal fun String.toBooleanStrictOrNull(): Boolean? = when {
+    this.equals("true", ignoreCase = true) -> true
+    this.equals("false", ignoreCase = true) -> false
+    else -> null
+}
